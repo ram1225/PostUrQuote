dd1cf45dc9ba2961375227f6df076964679842e5
ram1225 <geek.emp25@gmail.com>
2018-06-02 13:41:03 +0530

    Field validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-
+/*
+For email pattern validation: https://www.concretepage.com/angular-2/angular-2-4-email-validation-example
+For component based error validation https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/#displaying-the-validation-error-message
+*/
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,42 +12,36 @@ import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  
-  constructor(private formBuilder: FormBuilder) {  
-   }
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.createForm();
   }
 
- 
-
   createForm() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(null, {
-         validators: [Validators.required,Validators.email],
-         updateOn: 'blur'
-      }),
-      password: new FormControl(null, [Validators.required])
-   }, {updateOn: 'submit'})
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      password: new FormControl('', 
+      [Validators.required, Validators.minLength(5),Validators.maxLength(10)])
+    });
   }
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log('form submitted');
-    } else {
-      this.validateAllFormFields(this.loginForm); //{7}
-    }
+  get getemail() {
+    return this.loginForm.get('email');
   }
 
-  validateAllFormFields(formGroup: FormGroup) {         //{1}
-  Object.keys(formGroup.controls).forEach(field => {  //{2}
-    const control = formGroup.get(field);             //{3}
-    if (control instanceof FormControl) {             //{4}
-      control.markAsTouched({ onlySelf: true });
-    } else if (control instanceof FormGroup) {        //{5}
-      this.validateAllFormFields(control);            //{6}
-    }
-  });
-}
+  get getpassword() {
+    return this.loginForm.get('password');
+  }
+
+  submitForm(loginForm) {
+   
+      console.log('form submitted' + JSON.stringify(loginForm));
+   
+  }
+
+
 }
