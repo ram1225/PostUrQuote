a6aed5dc15b9f81ab3396bd9d334654b34b0f3ba
ram1225 <geek.emp25@gmail.com>
2018-06-02 16:56:01 +0530

    Added font-awesome icon,password validation,added tooltip using ngBoots

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
@@ -14,9 +17,13 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/* For ng bootstrap setup https://www.npmjs.com/package/@ng-bootstrap/ng-bootstrap */
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 /*
 For email pattern validation: https://www.concretepage.com/angular-2/angular-2-4-email-validation-example
 For component based error validation https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/#displaying-the-validation-error-message
+For password pattern: https://www.mkyong.com/regular-expressions/how-to-validate-password-with-regular-expression/
+
 */
 @Component({
   selector: 'app-login',
@@ -12,9 +14,20 @@ For component based error validation https://loiane.com/2017/08/angular-reactive
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-
+  emailPattern = "^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  passwordPattern = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+  ruleSet: string;
   constructor(private formBuilder: FormBuilder) {
+    /* declaring some rules 
+    For tooltip ng bootstrap : https://ng-bootstrap.github.io/#/components/tooltip/api
+    */
+    this.ruleSet= `
+     1) must contains one digit from 0-9  \n
+     2) must contains one lowercase characters \n
+     3) must contains one uppercase characters
+     4) must contains one special symbols in the list "@#$%"
+     5) length at least 6 characters and maximum of 20
+    `;
   }
 
   ngOnInit() {
@@ -24,8 +37,8 @@ export class LoginComponent implements OnInit {
   createForm() {
     this.loginForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
-      password: new FormControl('', 
-      [Validators.required, Validators.minLength(5),Validators.maxLength(10)])
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
     });
   }
 
@@ -38,9 +51,9 @@ export class LoginComponent implements OnInit {
   }
 
   submitForm(loginForm) {
-   
-      console.log('form submitted' + JSON.stringify(loginForm));
-   
+
+    console.log('form submitted' + JSON.stringify(loginForm));
+
   }
 
 
