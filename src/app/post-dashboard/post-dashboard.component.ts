7b4817559b6335fa1edc2975fe230b773f27b9ae
ram1225 <geek.emp25@gmail.com>
2018-06-04 23:56:09 +0530

    Change in aside,post, home component

--- src/app/post-dashboard/post-dashboard.component.ts
+++ src/app/post-dashboard/post-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-dashboard.component.css']
 })
 export class PostDashboardComponent implements OnInit {
-
-  constructor() { }
+  pattern: string = "[a-zA-Z0-9- ]+";
+  postForm: FormGroup;
+  
+  constructor() {
+    // this.message
+    // .valueChanges
+    // .debounceTime(200)
+    // .distinctUntilChanged()
+    //  .subscribe((event) => {
+   }
 
   ngOnInit() {
+   this.postForm= new FormGroup({
+    messageArea: new FormControl(null,[Validators.required,Validators.pattern(this.pattern)])
+   });
   }
 
+  get messageArea(){
+   return this.postForm.get('messageArea');
+  }
+
+  formSubmit(){
+    console.log(this.postForm.value);
+  }
 }
